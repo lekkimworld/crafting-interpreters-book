8f94499c78928820a3c8f51455e678f5e36baa92
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2024-12-26 22:28:33 +0100

    Scanner done

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,10 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
@@ -10,6 +13,27 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     public Scanner(String source) {
         this.source = source;
@@ -74,7 +98,8 @@ public class Scanner {
             case '/':
                 if (match('/')) {
                     // found comment
-                    while (peek() != '\n' && !isAtEnd()) advance();
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
                 } else {
                     addToken(SLASH);
                 }
@@ -90,14 +115,67 @@ public class Scanner {
                 string();
                 break;
             default:
-                Lox.error(this.line, "Unexpected character (" + c + ")");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(this.line, "Unexpected character (" + c + ")");
+                }
                 break;
         }
     }
 
+    private void identifier() {
+        while (isAlphaNumeric(peek())) {
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+            while (isDigit(peek()))
+                advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
     private void string() {
         while (peek() != '"' && !isAtEnd()) {
-            if (peek() == '\n') this.line++;
+            if (peek() == '\n')
+                this.line++;
             advance();
         }
 
@@ -110,18 +188,21 @@ public class Scanner {
         advance();
 
         // trim surrounding quotes
-        String value = this.source.substring(this.start+1, this.current-1);
+        String value = this.source.substring(this.start + 1, this.current - 1);
         addToken(STRING, value);
     }
 
     private char peek() {
-        if (this.isAtEnd()) return '\0';
+        if (this.isAtEnd())
+            return '\0';
         return this.source.charAt(this.current);
     }
 
     private boolean match(char expected) {
-        if (this.isAtEnd()) return false;
-        if (source.charAt(current) != expected) return false;
+        if (this.isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
 
         // next character is the expected one - consume it and return true
         this.current++;
@@ -130,7 +211,7 @@ public class Scanner {
 
     private char advance() {
         this.current++;
-        return this.source.charAt(this.current-1);
+        return this.source.charAt(this.current - 1);
     }
 
     private void addToken(TokenType type) {
